c08e50c753210b06492f9c22c41bf8154bb3c0f9
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-07-20 16:35:28 -0500

    [update]

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module makesite
 
 go 1.13
+
+require github.com/bregydoc/gtranslate v0.0.0-20191225024717-45b28d4fda86
+
+require (
+	github.com/robertkrimen/otto v0.0.0-20191219234010-c382bd3c16ff // indirect
+	golang.org/x/text v0.3.3
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+)
--- makesite.go
+++ makesite.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
+	"strings"
+	"text/template"
+
+	"github.com/bregydoc/gtranslate"
+	"golang.org/x/text/language"
+	"gopkg.in/russross/blackfriday.v2"
 )
 
-
 type post struct {
-	User string
+	User    string
 	Content string
 }
 
 func main() {
-	ogFlag := flag.String("", "first-post.txt", "")
+	ogFlag := flag.String("file", "first-post.txt", "define text")
+	dirFlag := flag.String("directory", "none", "generates all .txt files in directory")
+	outputDirFlag := flag.String("output", "templates/", "Generator output directory")
 	flag.Parse()
 
-	var fileName string = *ogFlag
-	fileName fileName[0:string.Index(*ogFlag, ".")] + ".html"
+	if *dirFlag == "none" {
+		runFile(*ogFlag, "txt_dir/")
+	} else {
+		runDir(*dirFlag, *outputDirFlag)
+	}
+}
 
-	var fileData string = readFile(*ogFlag)
-	renderTemplate("template.tmpl", fileData, fileName)
+func runFile(fileFlag, directory string) {
+
+	var fileName string = ogFlag
+
+	if fileName[strings.Index(fileFlag, "."):len(fileFlag)] != ".txt" {
+		return
+	}
+
+	if strings.Contains(strings.ToLower(fileFlag), ".md") {
+
+		var data string = readFile(directory + fileFlag)
+		tmpl := renderTemplate("template.tmpl", data, fileName)
+		output := blackfriday.Run(tmpl)
+		ioutil.WriteFile(output, ogFlag)
+
+		return
+	}
+
+	fileName = fileName[0:strings.Index(fileFlag, ".")] + ".html"
+
+	var data string = readFile(directory + fileFlag)
+	renderTemplate("template.tmpl", data, fileName)
 }
 
 func readFile() string {
@@ -27,31 +59,64 @@ func readFile() string {
 		panic(err)
 	}
 
-	fmt.Println(string(fileContents))
 	return string(fileContents)
 }
 
-func renderTemplate(tPath, textData, fileName string) string {
-	paths := []string{
-		tPath,
+func runDir(directory, output string) {
+
+	if directory[len(directory)-1] != "/"[0] {
+		directory += "/"
 	}
 
-	f, err := os.create(fileName)
+	files, err := ioutil.ReadDir(directory)
+
 	if err != nil {
 		panic(err)
 	}
 
-	temp, err := template.New(tPath).ParseFiles(paths...)
+	for _, file := range files {
+
+		if file.IsDir() == false {
+			runFile(file.Name(), directory)
+		} else {
+			runDir(directory+"/"+file.Name(), output)
+		}
+	}
+}
+
+func renderTemplate(tPath, textData, fileName string) {
+	paths := []string{
+		tPath,
+	}
+
+	f, err := os.create("templates/" + fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	t, err := template.New(tPath).ParseFiles(paths...)
 	if err != nil {
 		panic(err)
 	}
 
 	ogName := fileName[0:strings.Index(fileName, ".")]
 
-	err : t.Execute(f, post{textData, ogName})
+	txtTranslated := translateText(textData)
+
+	err = t.Execute(f, post{txtTranslated, ogName})
 	if err != nil {
 		panic(err)
 	}
 
 	f.Close()
+}
+
+func translateText(txtData string) string {
+
+	translated, err := gtranslate.Translate(txtData, language.English, language.French)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(translated)
 }
